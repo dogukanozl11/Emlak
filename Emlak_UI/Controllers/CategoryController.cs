ff06279b32ccd9f8d7f5d0246722fa44e69e70d2
Doğukan ÖZEL <dogukanozel692@gmail.com>
2024-07-11 05:46:20 +0300

    AdminCategory

--- Emlak_UI/Controllers/CategoryController.cs
+++ Emlak_UI/Controllers/CategoryController.cs
@@ -1,11 +1,29 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Emlak_UI.Dtos.CategoryDtos;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
 
 namespace Emlak_UI.Controllers
 {
     public class CategoryController : Controller
     {
-        public IActionResult Index()
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public CategoryController(IHttpClientFactory httpClientFactory)
         {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage = await client.GetAsync("https://localhost:44382/api/Categories");
+            if (responseMessage.IsSuccessStatusCode)//Http den bize gelen yanıtın başarılı olmasıyla bize bir veri döndürür.
+            {
+                var jsonData = await responseMessage.Content.ReadAsStringAsync();//Gelen içeriği String formatında oku.
+                var values = JsonConvert.DeserializeObject<List<ResultCategoryDto>>(jsonData);
+                return View(values);
+
+            }
             return View();
         }
     }
---
+++ Emlak_UI/ViewComponents/AdminLayout/_AdminLayoutNavbarComponentPartial.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emlak_UI.ViewComponents.AdminLayout
+{
+    public class _AdminLayoutNavbarComponentPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        { return View(); }
+    }
+}
