d7363c5dc3af16390c72d4e38ca6a9dc348fef3c
Doğukan ÖZEL <dogukanozel692@gmail.com>
2024-07-17 19:33:43 +0300

    StatisticsAPI

--- Emlak/Program.cs
+++ Emlak/Program.cs
@@ -5,6 +5,7 @@ using Emlak_Api.Repositories.EmployeeRepositories;
 using Emlak_Api.Repositories.PopularLocationRepositories;
 using Emlak_Api.Repositories.ProductRepository;
 using Emlak_Api.Repositories.ServiceRepository;
+using Emlak_Api.Repositories.StatisticRepositories;
 using Emlak_Api.Repositories.TestimonialRepositories;
 using Emlak_Api.Repositories.WhoWeAreRepository;
 
@@ -21,6 +22,7 @@ builder.Services.AddTransient<IBottomGridRepository, BottomGridRepository>();
 builder.Services.AddTransient<IPopularLocationRepository, PopularLocationRepository>();
 builder.Services.AddTransient<ITestimonialRepository, TestimonialRepository>();
 builder.Services.AddTransient<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddTransient<IStatisticsRepository, StatisticsRepository>();
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
