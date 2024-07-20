6e217afe35fa95a2ef912e2e311ed3c730800993
Doğukan ÖZEL <dogukanozel692@gmail.com>
2024-07-20 19:24:23 +0300

    AreasUserPanel

---
+++ Emlak_UI/Areas/EstateAgent/Controllers/MyAdvertsController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Emlak_UI.Areas.EstateAgent.Controllers
+{
+    public class MyAdvertsController : Controller
+    {
+        [Area("EstateAgent")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
